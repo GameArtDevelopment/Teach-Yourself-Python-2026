62059135eb73366bb551382518543260f9d98ba3
Joseph Prez <111929236+GameArtDevelopment@users.noreply.github.com>
2025-12-30 20:43:39 -0500

    variables

---
+++ python-fundamentals/variables.py
@@ -0,0 +1,6 @@
+name = "Joseph"  # name = string(str) text in quotes
+age = 23  # age = integer(int) whole numbers
+height = 5.10  # height = float(float) decimal numbers
+is_developer = True  # is_developer = boolean(bool) True or False
+
+# Python is a dynamically typed language. Types are not declared explicitly
